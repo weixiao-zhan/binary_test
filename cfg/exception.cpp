1a5d4ebad8de5a2315c8392207177a87c7a4dc68
Weixiao Zhan <weixiao.zhan00@gmail.com>
2022-08-18 21:12:11 -0700

    revise

--- cfg/exception.cpp
+++ cfg/exception.cpp
@@ -6,16 +6,16 @@ using namespace std;
 class parent_class {
 public:
     int* ptr;
-    parent_class(int n) {
+    parent_class() {
         printf("constructor of parent_class\n");
         ptr = new int;
-        *ptr = n;
+        *ptr = 10;
     }
     ~parent_class() {
         printf("destructor of parent_class (%d)\n", *ptr);
         delete ptr;
     }
-    void mod() {
+    void modify() {
         ++(*ptr);
     }
 };
@@ -24,22 +24,25 @@ class child_class: public parent_class {
 public:
     int* another_ptr;
     child_class()
-        : parent_class(1){
+        : parent_class(){
         printf("constructor of child_class\n");
         another_ptr = new int;
+        *another_ptr = 10;
     }
     ~child_class() {
-        printf("destructor of child_class (%d %d)\n", *ptr, *another_ptr);
+        printf("destructor of child_class (%d, %d)\n", *ptr, *another_ptr);
         delete another_ptr;
     }
-    void mod() {
-        --(*another_ptr);
+    void modify() {
+        --(*ptr);
+        ++(*another_ptr);
     }
 };
 
 void my_exception_easy() {
     try {
         child_class c = child_class();
+        c.modify();
         throw_int();
     } catch (int myNum) {
         printf("catched: %d\n", myNum);
@@ -48,13 +51,13 @@ void my_exception_easy() {
 
 void my_exception_nested() {
     try {
-        child_class c = child_class();
-        c.mod();
+        parent_class p = parent_class();
+        p.modify();
         
         throw_bool();
         
-        parent_class p = child_class();
-        p.mod();
+        child_class c = child_class();
+        c.modify();
     } catch (int myNum) {
         printf("catched: %d\n", myNum);
     } catch (bool myBool) {
